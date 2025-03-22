688a8f18db1212418df00010555d30fe15fe67e4
Kambodzio <urmothergaveme@gmail.com>
2025-03-22 22:28:12 +0100

    fixing styling

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
- 
+document.getElementById('ofertyButton').addEventListener('click', function() {
+    window.location.href = 'oferty.html';
+    });
+    // Tutaj możesz dodać więcej logiki JavaScript dla strony głównej, np. animacje, interakcje z elementami itp.
